0bdc61dd1a0af6adaaa88d8a213c4f53251dc459
Marten Seemann <martenseemann@gmail.com>
2019-11-16 16:43:55 +0800

    add some bandwidth reporting

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"math/big"
 	"net"
+	"sync/atomic"
 	"time"
 
 	quic "github.com/lucas-clemente/quic-go"
@@ -44,6 +46,78 @@ func main() {
 	}
 }
 
+func humanizeBytes(s uint64) string {
+	return humanateBytes(s, []string{"B", "kB", "MB", "GB", "TB", "PB", "EB"})
+}
+
+func humanizeBits(s uint64) string {
+	return humanateBytes(s, []string{"Bits", "kBits", "MBits", "GBits", "TBits", "PBits", "EBits"})
+}
+
+// see https://github.com/dustin/go-humanize/blob/master/bytes.go
+func humanateBytes(s uint64, sizes []string) string {
+	const base = 1000
+	if s < 10 {
+		return fmt.Sprintf("%d B", s)
+	}
+	e := math.Floor(math.Log(float64(s)) / math.Log(base))
+	suffix := sizes[int(e)]
+	val := math.Floor(float64(s)/math.Pow(base, e)*10+0.5) / 10
+	f := "%.2f %s"
+	if val < 10 {
+		f = "%.2f %s"
+	}
+
+	return fmt.Sprintf(f, val, suffix)
+}
+
+type bandwidthCounter struct {
+	startTime  time.Time
+	lastReport time.Time
+
+	total   uint64
+	counter uint64 // to be used as an atomic
+}
+
+func (c *bandwidthCounter) Add(n int) {
+	atomic.AddUint64(&c.total, uint64(n))
+	atomic.AddUint64(&c.counter, uint64(n))
+}
+
+func (c *bandwidthCounter) print(start, end time.Duration, n uint64) {
+	dur := end - start
+	bw := 8 * uint64(float64(n)/dur.Seconds())
+	fmt.Printf(" %.2f-%.2f\tsec\t%s\t%s/sec\n", start.Seconds(), end.Seconds(), humanizeBytes(n), humanizeBits(bw))
+}
+
+func (c *bandwidthCounter) Run(done <-chan struct{}) {
+	now := time.Now()
+	c.startTime = now
+	c.lastReport = now
+
+	fmt.Println("Interval\t\tTransfer\tBitrate")
+
+	ticker := time.NewTicker(time.Second)
+	for {
+		select {
+		case <-done:
+			fmt.Println("- - - - - - - - - - - - - - - - - - - - - - - - -")
+			fmt.Println("Interval\t\tTransfer\tBitrate")
+			c.print(0, time.Since(c.startTime), atomic.LoadUint64(&c.total))
+			return
+		case <-ticker.C:
+			now := time.Now()
+			n := atomic.SwapUint64(&c.counter, 0)
+			c.print(c.lastReport.Sub(c.startTime), now.Sub(c.startTime), n)
+			c.lastReport = now
+		}
+	}
+}
+
+func (c *bandwidthCounter) FinalReport() (startTime time.Time, n uint64) {
+	return c.startTime, atomic.LoadUint64(&c.counter)
+}
+
 func runServer(port int, duration time.Duration) error {
 	tlsConf, err := getTLSConfig()
 	if err != nil {
@@ -66,7 +140,7 @@ func runServer(port int, duration time.Duration) error {
 		return err
 	}
 	raddr := sess.RemoteAddr()
-	fmt.Printf("Accepted connection from %s", raddr.(*net.UDPAddr).String())
+	fmt.Printf("Accepted connection from %s\n", raddr.(*net.UDPAddr).String())
 
 	timer := time.AfterFunc(duration, func() {
 		sess.Close()
@@ -75,6 +149,9 @@ func runServer(port int, duration time.Duration) error {
 
 	var g errgroup.Group
 	var data [1 << 12]byte // 4 kbyte
+	var bc bandwidthCounter
+	go bc.Run(sess.Context().Done())
+
 	for i := 0; i < numStreams; i++ {
 		str, err := sess.OpenUniStream()
 		if err != nil {
@@ -82,7 +159,9 @@ func runServer(port int, duration time.Duration) error {
 		}
 		g.Go(func() error {
 			for {
-				if _, err := str.Write(data[:]); err != nil {
+				n, err := str.Write(data[:])
+				bc.Add(n)
+				if err != nil {
 					return err
 				}
 			}
@@ -99,7 +178,7 @@ func runClient(address string, port int, duration time.Duration) error {
 			InsecureSkipVerify: true,
 			NextProtos:         []string{"qperf"},
 		},
-		&quic.Config{},
+		&quic.Config{MaxIncomingUniStreams: 1000},
 	)
 	if err != nil {
 		return err
@@ -112,6 +191,9 @@ func runClient(address string, port int, duration time.Duration) error {
 	})
 	defer timer.Stop()
 
+	var bc bandwidthCounter
+	go bc.Run(sess.Context().Done())
+
 	var g errgroup.Group
 	for {
 		str, err := sess.AcceptUniStream(context.Background())
@@ -120,8 +202,13 @@ func runClient(address string, port int, duration time.Duration) error {
 		}
 
 		g.Go(func() error {
-			_, err := io.Copy(ioutil.Discard, str)
-			return err
+			for {
+				n, err := io.CopyN(ioutil.Discard, str, 1<<9)
+				bc.Add(int(n))
+				if err != nil {
+					return err
+				}
+			}
 		})
 	}
 
